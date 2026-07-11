32225f3ef1f8c8eebaef8b723a01758793e9a07c
kenneth <joshuabecaro@gmail.com>
2026-07-11 22:05:58 +0800

    feat(auth): add tanstack query provider to auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,17 @@
 
 import Header from "@/features/marketing/components/Header";
 import ProductPreview from "./_components/ProductPreview";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 export default function AuthLayout({
   children,
@@ -9,18 +20,20 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div
-      data-theme="wordscribe"
-      className="min-h-screen flex flex-col bg-base-100 antialiased font-sans select-none"
-    >
-      <Header />
-      <main className="grow grid grid-cols-1 lg:grid-cols-2">
-        <div className="flex flex-col justify-center items-center px-6 py-12 md:px-12 bg-base-100">
-          <div className="w-full max-w-md">{children}</div>
-        </div>
+    <QueryClientProvider client={queryClient}>
+      <div
+        data-theme="wordscribe"
+        className="min-h-screen flex flex-col bg-base-100 antialiased font-sans select-none"
+      >
+        <Header />
+        <main className="grow grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col justify-center items-center px-6 py-12 md:px-12 bg-base-100">
+            <div className="w-full max-w-md">{children}</div>
+          </div>
 
-        <ProductPreview />
-      </main>
-    </div>
+          <ProductPreview />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }

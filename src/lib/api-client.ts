610b1a33141472bbd4723a453359299bee5c1990
kenneth <joshuabecaro@gmail.com>
2026-07-11 22:25:29 +0800

    feat: add api client lib

---
+++ src/lib/api-client.ts
@@ -0,0 +1,45 @@
+import Axios, { InternalAxiosRequestConfig } from "axios";
+
+import { paths } from "@/config/paths";
+import { useNotifications } from "@/components/notifications/notifications-store";
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = "application/json";
+  }
+
+  config.withCredentials = true;
+  return config;
+}
+
+export const api = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+api.interceptors.request.use(authRequestInterceptor);
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message =
+      error.response?.data?.detail ||
+      error.response?.data?.message ||
+      error.message;
+
+    useNotifications.getState().addNotification({
+      type: "error",
+      title: "Error",
+      message,
+    });
+
+    if (error.response?.status === 401) {
+      const searchParams = new URLSearchParams();
+      const redirectTo =
+        searchParams.get("redirectTo") || window.location.pathname;
+      window.location.href = paths.auth.login.getHref(redirectTo);
+    }
+
+    return Promise.reject(error);
+  },
+);

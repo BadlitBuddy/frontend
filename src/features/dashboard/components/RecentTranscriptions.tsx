b1eef690c98a67b62b2a656383e507099231cd5c
k02_10 <joshuabecaro@gmail.com>
2026-08-23 13:51:18 +0800

    refactor: Move Download Menu component to own file

--- src/features/dashboard/components/RecentTranscriptions.tsx
+++ src/features/dashboard/components/RecentTranscriptions.tsx
@@ -5,25 +5,16 @@ import {
   Badge,
   Box,
   Group,
-  Loader,
-  Menu,
   Skeleton,
   Table,
   Text,
 } from "@mantine/core";
-import {
-  AudioWaveformIcon,
-  CaptionsIcon,
-  DownloadIcon,
-  FileBracesCornerIcon,
-  FileTextIcon,
-} from "lucide-react";
+import { AudioWaveformIcon } from "lucide-react";
 import Link from "next/link";
-import { useQueryClient } from "@tanstack/react-query";
 import { useGetTranscripts } from "@/features/transcripts/api/get-transcripts";
 import { TranscriptionJobStatus } from "../types";
 import classes from "../styles/RecentTranscriptions.module.css";
-import { useTranscriptionDownload } from "@/features/transcripts/hooks/useTranscriptionDownload";
+import { DownloadMenu } from "@/components/DownloadMenu";
 
 function StatusBadge({ status }: { status: TranscriptionJobStatus }) {
   if (status === TranscriptionJobStatus.Completed) {
@@ -130,82 +121,6 @@ function RowAction({ transcriptId, fileName, status }: RowActionProps) {
         <DownloadMenu transcriptId={transcriptId} />
       </Box>
     </Group>
-  );
-}
-
-type DownloadMenuProps = {
-  transcriptId: string;
-};
-function DownloadMenu({ transcriptId }: DownloadMenuProps) {
-  const queryClient = useQueryClient();
-
-  const { handleDownload, downloading } = useTranscriptionDownload({
-    transcriptId,
-    queryClient,
-  });
-
-  const isLoading = (format: string) => downloading === format;
-
-  return (
-    <Menu shadow="md" width={150}>
-      <Menu.Target>
-        <Box component="span" style={{ display: "inline-flex" }}>
-          {downloading ? (
-            <Loader size={16} color="slate.5" />
-          ) : (
-            <DownloadIcon size={16} />
-          )}
-        </Box>
-      </Menu.Target>
-
-      <Menu.Dropdown>
-        <Menu.Label>Export Formats</Menu.Label>
-
-        <Menu.Item
-          leftSection={
-            isLoading("json") ? (
-              <Loader size={16} />
-            ) : (
-              <FileBracesCornerIcon size={16} />
-            )
-          }
-          onClick={() => handleDownload("json")}
-          disabled={!!downloading}
-        >
-          .json
-        </Menu.Item>
-
-        <Menu.Item
-          leftSection={
-            isLoading("srt") ? <Loader size={16} /> : <CaptionsIcon size={16} />
-          }
-          onClick={() => handleDownload("srt")}
-          disabled={!!downloading}
-        >
-          .srt
-        </Menu.Item>
-
-        <Menu.Item
-          leftSection={
-            isLoading("vtt") ? <Loader size={16} /> : <CaptionsIcon size={16} />
-          }
-          onClick={() => handleDownload("vtt")}
-          disabled={!!downloading}
-        >
-          .vtt
-        </Menu.Item>
-
-        <Menu.Item
-          leftSection={
-            isLoading("txt") ? <Loader size={16} /> : <FileTextIcon size={16} />
-          }
-          onClick={() => handleDownload("txt")}
-          disabled={!!downloading}
-        >
-          .txt
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
   );
 }
 

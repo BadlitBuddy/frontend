15940815a2c581bbc94c73d1b63abdc203915bb5
k02_10 <joshuabecaro@gmail.com>
2026-08-31 22:40:31 +0800

    refactor: remove unused imports and components from ProfileSection

--- src/features/settings/components/ProfileSection.tsx
+++ src/features/settings/components/ProfileSection.tsx
@@ -1,19 +1,6 @@
 "use client";
 
-import {
-  Anchor,
-  Box,
-  Button,
-  Divider,
-  Grid,
-  Group,
-  Paper,
-  PasswordInput,
-  Stack,
-  Text,
-  TextInput,
-} from "@mantine/core";
-import classes from "../styles/settings.module.css";
+import { Grid, Paper, Stack, Text, TextInput } from "@mantine/core";
 
 export function ProfileSection() {
   return (
@@ -43,42 +30,6 @@ export function ProfileSection() {
               />
             </Grid.Col>
           </Grid>
-
-          <Divider color="slate.1" />
-
-          <Box>
-            <Group justify="space-between" align="center">
-              <PasswordInput
-                label="Password"
-                defaultValue="password123"
-                size="sm"
-                style={{ flex: 1 }}
-                c="slate.7"
-                mb="4"
-              />
-            </Group>
-          </Box>
-
-          <Divider color="slate.1" />
-
-          <Group justify="flex-end">
-            <Button
-              variant="transparent"
-              size="sm"
-              className={classes.inlineLink}
-            >
-              Change Password
-            </Button>
-            <Anchor
-              size="sm"
-              c="error.6"
-              fw={500}
-              underline="hover"
-              style={{ cursor: "pointer" }}
-            >
-              Delete account
-            </Anchor>
-          </Group>
         </Stack>
       </Paper>
     </Stack>
